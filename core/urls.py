215d08ffb1dbb569f07fa00c9042e18c0be3cd85
indsouza <igornevesdesouza@gmail.com>
2025-11-01 09:25:53 -0300

    Salvando as informações do formulário no banco de dados

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import index,contato,produto
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',index,name='index'),
